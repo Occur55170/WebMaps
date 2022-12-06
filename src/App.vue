9db9424686ba4729020f9450f7015eebd6e47d7d
Occur55170 <42513736+Occur55170@users.noreply.github.com>
2022-12-06 23:48:21 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import WebMap from './components/WebMap.vue'
+// import searchBar from './components/searchBar.vue'
+import Tool from './components/Tool.vue'
+
+  const state = {
+    pickedContract: '123'
+  }
+</script>
+
+<template>
+  <div class="main">
+    <Tool />
+  </div>
+  <WebMap class="mapContent" />
+</template>
+
+<style scoped>
+.main{
+  position: fixed;
+  z-index: 220;
+}
+.mapContent{
+  position: fixed;
+  top: 0;
+  left: 0px;
+  outline: none;
+}
+</style>
---
+++ src/components/WebMap.vue
@@ -0,0 +1,43 @@
+<script>
+  import 'leaflet/dist/leaflet.css'
+  import L from 'leaflet'
+  import {onMounted} from 'vue'
+
+  export default ({
+    setup(props, { emit }){
+      const state = ({
+          pickedContract: '999'
+      })
+
+      console.log(12399)
+
+      onMounted(()=>{
+        var map = L.map('map').setView([24.801583, 120.971859], 17);
+        L.marker([24.80340415977538, 120.96884449432741],{alt: '1號門市'}).addTo(map).bindPopup('Kyiv, Ukraine is the birthplace of Leaflet!');
+        L.marker([24.80315093346662, 120.97174134060748],{alt: '2號門市'}).addTo(map).bindPopup('Kyiv, Ukraine is the birthplace of Leaflet!');
+        L.marker([24.802030912259397, 120.9689732570909],{alt: '3號門市'}).addTo(map).bindPopup('Kyiv, Ukraine is the birthplace of Leaflet!');
+        L.marker([24.801027681204214, 120.97517456219623],{alt: '4號門市'}).addTo(map).bindPopup('Kyiv, Ukraine is the birthplace of Leaflet!');
+        L.marker([24.804533900189952, 120.973543868621],{alt: '5號門市'}).addTo(map).bindPopup('Kyiv, Ukraine is the birthplace of Leaflet!');
+        L.marker([24.801034199227438, 120.96738707084371],{alt: '6號門市'}).addTo(map).bindPopup('Kyiv, Ukraine is the birthplace of Leaflet!');
+        L.marker([24.80183734266878, 120.96384710331658],{alt: '7號門市'}).addTo(map).bindPopup('Kyiv, Ukraine is the birthplace of Leaflet!');
+        L.marker([24.79826565221393, 120.97370317437601],{alt: '8號門市'}).addTo(map).bindPopup('Kyiv, Ukraine is the birthplace of Leaflet!');
+
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+          attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      }).addTo(map);
+      })
+    }
+  })
+</script>
+
+<template>
+  <div id="map">
+  </div>
+</template>
+
+<style scoped>
+#map{
+  width: 100%;
+  height: 100%;
+}
+</style>
