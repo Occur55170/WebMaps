ea80a5ce4e3ecb8c84dcb6a389591357e0a68f70
Occur <tw17055@gmail.com>
2023-06-14 17:42:29 +0800

    add Eslint

--- src/App.vue
+++ src/App.vue
@@ -24,22 +24,12 @@ export default {
 </template>
 
 <style lang="sass">
-// @import './assets/styles/all.module.scss'
-.mapContent
-    height: 100vh
-    width: 100vw
 .main
     position: fixed
     top: 0
     left: 0
     height: 100%
     width: 100%
-
-.LayoutTool
-    position: absolute
-    top: 0
-    left: 0
-    z-index: 220
 .lightWrap
     z-index: 999
     position: fixed
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
-//vite.config.js
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
-import Icons from 'unplugin-icons/vite'//一樣先引入套件
+import Icons from 'unplugin-icons/vite'// 一樣先引入套件
 import IconsResolver from 'unplugin-icons/resolver'
-import cesium from 'vite-plugin-cesium';
-import path from 'path'   // 需安装此模块
+import cesium from 'vite-plugin-cesium'
+import path from 'path' // 需安装此模块
 export default defineConfig(({ command, mode, ssrBuild }) => {
     // 根據當前工作目錄中的 `mode` 加載 .env 文件
     // 設置第三個參數為 '' 來加載所有環境變量，而不管是否有 `VITE_` 前綴。
@@ -23,7 +22,7 @@ export default defineConfig(({ command, mode, ssrBuild }) => {
                 }),
             }),
             {
-              includePaths: ['./src/styles']
+                includePaths: ['./src/styles']
             }
         ],
         css: {
@@ -35,11 +34,11 @@ export default defineConfig(({ command, mode, ssrBuild }) => {
             }
         },
         configureWebpack: {
-            externals: { 'jquery': '$' },
+            externals: { jquery: '$' },
             resolve: {
                 alias: {
                     '@': fileURLToPath(new URL('./src', import.meta.url)),
-                    'vue': '@vue/runtime-dom'
+                    vue: '@vue/runtime-dom'
                 }
             }
         },
@@ -55,6 +54,6 @@ export default defineConfig(({ command, mode, ssrBuild }) => {
                 '@': path.resolve(__dirname, 'src')
             }
         },
-        envDir: path.resolve(__dirname, "./env")
+        envDir: path.resolve(__dirname, './env')
     }
 })
