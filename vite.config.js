d2af32b3378dfb1eb6e97bfa42f54ba47a34ae89
Occur <tw17055@gmail.com>
2023-08-22 16:33:22 +0800

    pref config

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import Icons from 'unplugin-icons/vite'// 一樣先引入套件
 import IconsResolver from 'unplugin-icons/resolver'
 import cesium from 'vite-plugin-cesium'
 import path from 'path' // 需安装此模块
-export default defineConfig(({ command, mode, ssrBuild }) => {
+export default defineConfig(({ mode }) => {
     // 根據當前工作目錄中的 `mode` 加載 .env 文件
     // 設置第三個參數為 '' 來加載所有環境變量，而不管是否有 `VITE_` 前綴。
     const ENV = loadEnv(mode, process.cwd(), '')
@@ -33,18 +33,6 @@ export default defineConfig(({ command, mode, ssrBuild }) => {
                 }
             }
         },
-        configureWebpack: {
-            externals: { jquery: '$' },
-            resolve: {
-                alias: {
-                    '@': fileURLToPath(new URL('./src', import.meta.url)),
-                    vue: '@vue/runtime-dom'
-                }
-            }
-        },
-        // 輸出路徑設定
-        // base: env.VITE_BASE, //不同模式設定不同路徑
-        // base: './mapDemo',
         base: '/Map_Demo/',
         build: {
             sourcemap: true
