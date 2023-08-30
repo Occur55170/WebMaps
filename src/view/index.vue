44f3819352d5f0b126bae7ad89b09ef1b094c2d1
Occur55170 <42513736+Occur55170@users.noreply.github.com>
2023-08-31 01:17:53 +0800

    feat: index img

--- src/view/index.vue
+++ src/view/index.vue
@@ -18,15 +18,24 @@ export default {
 
 <template>
   <div class="wrap">
-    <div class="startCon">
+    <div class="startCon d-flex justify-content-between">
         <!-- w-100 w-sm-50 -->
-        <div class="content">
+        <div class="content px-0 px-sm-5 position-relative">
             <img src="@/assets/img/logo.svg" class="logo w-100 w-sm-80" alt="">
             <p class="fs-sm-6 fs-5">保護您與家人，防範災害。提供最新安全資訊、防護措施和應對指南。建立安全居住環境，增強防災能力。攜手合作，共築安全明天！</p>
             <a href="" class="d-inline-block" @click.prevent="route.push({name: 'map'})">
                 <img src="@/assets/img/enterMapBtn.svg" alt="">
             </a>
         </div>
+        <div class="overLay">
+            <img src="@/assets/img/indexOverlay.svg" class="d-block w-100" alt="">
+        </div>
+    </div>
+    <div class="tip d-flex justify-content-around py-3 fixed-bottom" style="background: rgb(255, 255, 255, 0.5);">
+        <span>台灣整合防災工程技術顧問有限公司</span>
+        <span>公司地址：236新北市土城區中央路三段281巷15弄14號</span>
+        <span>E-mail：salahayo3192@gmail.com</span>
+        <span>Copyright © 2023 台灣整合防災工程技術顧問有限公司</span>
     </div>
   </div>
 </template>
@@ -36,39 +45,68 @@ export default {
 .wrap
     background-image: url('@/assets/img/indexBackground.svg')
     background-repeat: no-repeat
-    background-size: cover
+    background-size: 100% auto
     background-position: bottom
     box-sizing: border-box
-    padding: 100px
+    padding: 5%
 .startCon
-    max-width: 900px
+    width: 95%
     .logo
         margin-bottom: 40px
     .content
         box-sizing: border-box
-        padding: 0 0 0 60px
+        padding: 60px
         margin: 50px 0
+        width: 50%
         &>p
             margin-bottom: 60px
-    &::before
-        content: ''
-        background-image: url('@/assets/img/indexHead-be.svg')
-        background-repeat: no-repeat
-        display: block
+        &::before
+            content: ''
+            background-image: url('@/assets/img/indexTitle-top.svg')
+            background-repeat: no-repeat
+            transform: scaleY(-1)
+            display: block
+            width: 100%
+            height: 30px
+            position: absolute
+            left: 0
+            top: 0
+        &::after
+            content: ''
+            background-image: url('@/assets/img/indexTitle-bottom.svg')
+            background-repeat: no-repeat
+            background-position: right
+            display: block
+            width: 100%
+            height: 30px
+            position: absolute
+            left: 0
+            bottom: 0
+    .overLay
+        transform: scale(1.1)
+@media (max-width: 1700px)
+    .wrap
+        background-size: 100% auto
+        background-position: bottom
+    .startCon
         width: 100%
-        height: 30px
-    &::after
-        content: ''
-        background-image: url('@/assets/img/indexHead-af.svg')
-        background-repeat: no-repeat
-        display: block
-        width: 100%
-        height: 30px
-        background-position: right
+        padding: 30px 0 30px 30px
+        .content
+            width: 55%
+        .overLay
+            width: 40%
+            transform: unset
 @media (max-width: 1700px)
     .startCon
-        max-width: 60%
-        padding: 30px
+        width: 100%
+        padding: 30px 0 30px 30px
+        .content
+            width: 60%
+        .overLay
+            width: 40%
+            transform: unset
+            img
+                width: 100%
 
 @media (max-width: 600px)
     .wrap
@@ -76,27 +114,35 @@ export default {
         background-size: cover
         padding: 10px
     .startCon
-        max-width: 900px
-        padding: 30px
+        width: 100%
+        padding: 10px
         .logo
-            margin-bottom: 0
+            margin:10px 0
+            width: 70% !important
         .content
             box-sizing: border-box
-            padding: 0
-            margin: 30px 0
+            padding: 30px 0
+            margin: 0
             text-align: center
+            width: 100%
             &>p
-                margin: 40px 0
+                margin: 20px 0
                 text-align: left
                 line-height: 1.6
-        &::before
-            content: ''
-            background-image: url('@/assets/img/indexHead-be.svg')
-            background-repeat: no-repeat
-            background-size: contain
-            display: block
-            width: 70%
-            height: 16px
-        &::after
+            &::before
+                content: ''
+                background-image: url('@/assets/img/indexTitle-top.svg')
+                background-repeat: no-repeat
+                background-size: contain
+                display: block
+                width: 70%
+                height: 16px
+            &::after
+                display: none
+            &>a
+                width: 90%
+                img
+                    width: 100%
+        .overLay
             display: none
 </style>
