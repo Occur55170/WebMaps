2251dd2755bcd20370805531a77f181cd9e1983d
Occur55170 <tw17055@gmail.com>
2026-04-11 15:27:49 +0800

    refactor: move Carousel component to global directory and remove unused vue-carousel dependency

--- src/main.js
+++ src/main.js
@@ -9,12 +9,10 @@ import componentRegister from './config/componentRegister'
 
 import router from '@/router/index.js'
 
-import VueCarousel from 'vue-carousel'
 import store from '@/store/index.js'
 
 createApp(App)
     .use(router)
-    .use(VueCarousel)
     .use(componentRegister)
     .use(store)
     .mount('#app')
